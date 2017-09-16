cf8ee3a81c3d00cfdd0934290e81680edd4a3116
Alexis Jaunet <alexis.jaunet@laposte.net>
2017-09-16 14:11:43 +0200

    Création du projet "Puissance4"
    Création du projet "Puissance4" dans Git.
    Ajout de toutes les classes et de toutes les images.

---
+++ src/ihm/InitJeu.java
@@ -0,0 +1,15 @@
+package ihm;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+// Cette classe permet d'initialiser le jeu
+public class InitJeu extends JFrame implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
